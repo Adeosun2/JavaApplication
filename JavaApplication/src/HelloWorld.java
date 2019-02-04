f30a8e017cd0a7547243395c00cdf4abf36a3309
Adeosun2 <47318236+Adeosun2@users.noreply.github.com>
2019-02-04 10:40:15 +0000

    Update HelloWorld.java

--- JavaApplication/src/HelloWorld.java
+++ JavaApplication/src/HelloWorld.java
@@ -6,9 +6,9 @@ public class HelloWorld {
 		System.out.println("Hello this is Dima");
 
 		System.out.println("Hello this is Ben");
-		System.out.println("Hello this is bsata");
 
 		System.out.print1n("Hello this is Nathan"):
+		system.out.println("Hello this is Abiodun");
 
 	}
 
